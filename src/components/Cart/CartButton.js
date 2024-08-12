068f53533603cf981594cfe18b7ed1d62c8b5aa4
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-12 11:49:25 +0530

    dispatching toggle operation

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const uiDispatch = useDispatch(uiActions);
+
+  function toggleHandler() {
+    uiDispatch(uiActions.toggle());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
