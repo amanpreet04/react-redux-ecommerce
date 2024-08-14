a04afa5b43903d5c7693728ec8f9a73fc7040a65
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-14 10:43:12 +0530

    Added notification reducer

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { visible: false };
+const initialState = { visible: false, notification: null };
 const uiSlice = createSlice({
   name: "ui",
   initialState,
   reducers: {
     toggle(state) {
       state.visible = !state.visible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status, //pending error success
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
