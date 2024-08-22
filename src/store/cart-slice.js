d554d5d16bbe6ebe06bcae4c88183ae094259b92
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-22 22:44:22 +0530

    moved action thunk to separate file

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const initialState = {
   items: [],
@@ -39,48 +40,6 @@ const cartSlice = createSlice({
     },
   },
 });
-export const sendCartData = async (cart) => {
-  return async () => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending Cart Data",
-      })
-    );
-    const sendReq = async () => {
-      const response = await fetch(
-        "https://test-e3bac-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error("Something went wrong");
-      }
-    };
-
-    try {
-      await sendReq();
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sent Cart Data Successfully",
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending Cart Data Failed",
-        })
-      );
-    }
-  };
-};
 export const cartSliceActions = cartSlice.actions;
 export default cartSlice.reducer;
