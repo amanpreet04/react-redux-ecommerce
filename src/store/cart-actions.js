e80ee5f7f7968793f91894809676386e9ab64c84
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-23 22:34:10 +0530

    Added fetching thunk

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,3 +1,4 @@
+import { cartSliceActions } from "./cart-slice";
 import { uiActions } from "./ui-slice";
 
 export const sendCartData = async (cart) => {
@@ -38,6 +39,36 @@ export const sendCartData = async (cart) => {
           status: "error",
           title: "Error",
           message: "Sending Cart Data Failed",
+        })
+      );
+    }
+  };
+};
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://test-e3bac-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data");
+      }
+
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartSliceActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Fetching Cart Data Failed",
         })
       );
     }
